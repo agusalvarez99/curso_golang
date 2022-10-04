cfb5d99e1ce1a95caa52425127357daca63d7568
Agustín Ezequiel Alvarez <agusalvarez99@hotmail.com>
2022-10-04 15:01:11 -0300

    tipos de datos

--- proyecto/tipoDatos.go
+++ proyecto/tipoDatos.go
@@ -11,8 +11,32 @@ func varBool() {
 	fmt.Println(todoOK)
 }
 
+func tiposNum(entero int, decimal float32) float32 {
+	//fmt.Printf("Entero: %v - Decimal: %v\n", entero, decimal)
+	return (float32(entero) + decimal)
+}
+func tiposString(palabra string) {
+	fmt.Println(palabra)
+}
+
+func animal() {
+	var animal string
+	var edad int
+
+	fmt.Println("¿Cual es tu animal favorito?")
+	fmt.Scanf("%s\n", &animal)
+
+	fmt.Println("¿Que edad tiene?")
+	fmt.Scanf("%d\n", &edad)
+
+	fmt.Println("Mi animal favorito es el "+animal+" y tiene", edad, "años")
+}
+
 func main() {
-	var txt string = "El resultado es "
-	fmt.Println(txt, suma(5, 6))
-	varBool()
+	//var txt string = "El resultado es "
+	//fmt.Println(txt, suma(5, 6))
+	//varBool()
+	//println(tiposNum(5, 4.8))
+	//tiposString("hola")
+	animal()
 }
